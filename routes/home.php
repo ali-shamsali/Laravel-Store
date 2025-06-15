e695636fa370c1fedaadc49235c132f39b86d7e6
ali-shamsali <alishamsali2079@gmail.com>
2025-06-15 21:36:58 +0330

    Replace Laravel Jetstream with Laravel UI for auth scaffolding; removed Jetstream related files and configurations

--- routes/home.php
+++ routes/home.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/',  \App\Livewire\Home\Home\Index::class)->name('home.home');
+Auth::routes();
 
 Route::middleware([
     'auth:sanctum',
