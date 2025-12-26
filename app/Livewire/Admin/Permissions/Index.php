22c75b430db716cbc93b523ecb20fb4771223cb6
ali-shamsali <alishamsali2079@gmail.com>
2025-12-26 22:09:40 +0330

    Create permissions view

--- app/Livewire/Admin/Permissions/Index.php
+++ app/Livewire/Admin/Permissions/Index.php
@@ -2,12 +2,74 @@
 
 namespace App\Livewire\Admin\Permissions;
 
+use App\Models\admin\permissions\role;
 use Livewire\Component;
+use App\Models\Admin\Log;
+use App\Models\admin\permissions\permission;
 
 class Index extends Component
 {
+    public $title, $desc, $selectedPermissions = [];
+    public role $role;
+    public $search = '';
+    public $deleteId;
+
+    public function mount()
+    {
+        $this->role = new role();
+    }
+
+    protected $rules = [
+        'title' => 'required',
+        'desc'  => 'required',
+        'selectedPermissions' => 'nullable|array',
+    ];
+
+    public function roleForm()
+    {
+        $this->validate();
+
+        $this->role->title = $this->title;
+        $this->role->desc  = $this->desc;
+        $this->role->save();
+
+        if ($this->selectedPermissions) {
+            $this->role->permissions()->sync($this->selectedPermissions);
+        }
+
+        $this->role = new role();
+
+        session()->flash('message', 'نقش با موفقیت ذخیره شد!');
+        $desc = 'نقش توسط کاربر ذخیره شد';
+        Log::MakeLog('insert', $desc);
+
+        $this->reset(['title', 'desc', 'selectedPermissions']);
+    }
+
+    public function deleteRole($id)
+    {
+        $this->deleteId = $id;
+    }
+
+    public function deleteRecord($id)
+    {
+        $role = role::find($id);
+        $role->permissions()->detach();
+        $role->user()->detach();
+        $role->delete();
+        $desc = 'نقش با موفقیت توسط کاربر حذف شد!';
+        Log::MakeLog('delete', $desc);
+    }
+
     public function render()
     {
-        return view('livewire.admin.permissions.index');
+        if ($this->search != '') {
+            $roles = role::where('title', 'like', '%' . $this->search . '%')->paginate(2);
+            $permissions = permission::all();
+        } else {
+            $roles = role::all();
+            $permissions = permission::all();
+        }
+        return view('livewire.admin.permissions.index', compact('roles', 'permissions'));
     }
 }
--- resources/views/livewire/admin/permissions/index.blade.php
+++ resources/views/livewire/admin/permissions/index.blade.php
@@ -1,3 +1,92 @@
+@section('title', 'سطح دسترسی ها')
 <div>
-    {{-- Stop trying to control. --}}
+    <div class="main-content">
+        <div class="data-table-area">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xl-4 box-margin height-card">
+                        <div class="card card-body">
+                            <h4 class="card-title">افزودن سطح دسترسی</h4>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-12 col-xs-12">
+                                    <form wire:submit.prevent="roleForm">
+                                        <div class="form-group">
+                                            <label for="title">عنوان دسترسی (لاتین):</label>
+                                            <input wire:model.defer='title' type="text" class="form-control" id="title">
+                                            @error('title') <span class="text-danger">{{ $message }}</span> @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="desc">توضیحات دسترسی:</label>
+                                            <input wire:model.defer='desc' type="text" class="form-control" id="desc">
+                                            @error('desc') <span class="text-danger">{{ $message }}</span> @enderror
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="permissions">سطح دسترسی:</label>
+                                            <select class="form-control" multiple wire:model.lazy='selectedPermissions'>
+                                                @foreach ($permissions as $permission)
+                                                    <option value="{{ $permission->id }}">{{ $permission->title }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <button type="submit" class="btn btn-outline-success mb-2 mr-2">
+                                            <i class="fa fa-save"></i> ذخیره
+                                        </button>
+
+                                        @if (session()->has('message'))
+                                            <div class="alert alert-success mt-2">
+                                                {{ session('message') }}
+                                            </div>
+                                        @endif
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-lg-8 box-margin">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title mb-2">لیست سطح دسترسی ها</h4>
+                                <hr>
+                                <table class="table table-striped dt-responsive nowrap w-100">
+                                    <thead>
+                                        <tr>
+                                            <th>عنوان دسترسی</th>
+                                            <th>توضیحات نقش</th>
+                                            <th>سطح نقش</th>
+                                            <th>عملیات</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($permissions as $permission)
+                                            <tr>
+                                                <td>{{ $permission->title }}</td>
+                                                <td>{{ $permission->desc }}</td>
+                                                <td>
+                                                    @foreach ($permission->permissions as $permission)
+                                                        <span class="badge badge-info">{{ $role->title }}</span>
+                                                    @endforeach
+                                                </td>
+                                                <td>
+                                                    <a href="javascript:void(0);" class="action-icon">
+                                                        <i class="zmdi zmdi-edit zmdi-custom"></i>
+                                                    </a>
+                                                    <button wire:click='deleteRecord({{ $permission->id }})'
+                                                        data-toggle="modal" data-target="#exampleModal"
+                                                        class="action-icon"> <i
+                                                            class="zmdi zmdi-delete zmdi-custom"></i>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
